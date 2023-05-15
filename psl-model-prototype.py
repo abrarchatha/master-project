ee3e644ac80d0630b57b832bd3928f03461c8551
Abrar Ahmad Chatha <ca21acu>
2023-05-15 16:25:19 +0100

    Updated the model

--- psl-model-prototype.py
+++ psl-model-prototype.py
@@ -23,3 +23,22 @@ new_dataframe = pandas.get_dummies(data=dataframe, columns=['team_1', 'team_2'])
 new_dataframe = new_dataframe[['psl_year','team_1_Islamabad United', 'team_1_Quetta Gladiators', 'team_1_Lahore Qalandars','team_1_Karachi Kings', 'team_1_Peshawar Zalmi', 'team_1_Multan Sultans'
 ,'team_2_Islamabad United', 'team_2_Quetta Gladiators', 'team_2_Lahore Qalandars','team_2_Karachi Kings', 'team_2_Peshawar Zalmi', 'team_2_Multan Sultans',
 'inning','over', 'ball', 'total_runs', 'wickets']]
+
+# Train and Test Data
+xAxisTrain = new_dataframe.drop(labels='total_runs', axis=1)[new_dataframe['psl_year'] <= 2019]
+xAxisTest = new_dataframe.drop(labels='total_runs', axis=1)[new_dataframe['psl_year'] >= 2020]
+yAxisTrain = new_dataframe[new_dataframe['psl_year'] <= 2019]['total_runs']
+yAxisTest = new_dataframe[new_dataframe['psl_year'] >= 2020]['total_runs']
+xAxisTrain.drop(labels='psl_year', axis=True, inplace=True)
+xAxisTest.drop(labels='psl_year', axis=True, inplace=True)
+
+
+#Model
+from sklearn.linear_model import LinearRegression
+mlr = LinearRegression()
+mlr.fit(xAxisTrain,yAxisTrain)
+
+
+# pickle File
+file = 'psl-model.pkl'
+pickle.dump(mlr, open(file, 'wb'))
